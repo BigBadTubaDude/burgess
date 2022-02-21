4dbd4921ebe66d3f063bb58663e94af532d59361
BigBadTubaDude <cralex0307@gmail.com>
2022-02-21 13:49:26 -0500

    added about page

---
+++ scroller.js
@@ -0,0 +1,9 @@
+// window.addEventListener('scroll', function() {myFunction()});
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
